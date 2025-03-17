12294688efce6b1ebf7ec9590f04968cd41328dd
Rafal Wizen <rafal.wizen@gmail.com>
2025-03-17 21:44:57 +0100

    Prepare basic stylings

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
-import { Text, View } from "react-native";
+import {Text, View} from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hi.</Text>
-    </View>
-  );
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-5xl text-accent font bold">Hi.</Text>
+        </View>
+    );
 }
