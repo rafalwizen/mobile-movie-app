1e1c5b93ed16b70c2db5c897d7f2df707742cce8
Rafal Wizen <rafal.wizen@gmail.com>
2025-03-26 18:56:16 +0100

    Fix SearchBar.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
     return (
         <View className="flex-1 bg-primary">
             <Image source={images.bg} className="absolute w-full z-0"/>
-            <ScrollView className="flex-1 pz-5"
+            <ScrollView className="flex-1 px-5"
                         showsVerticalScrollIndicator={false}
                         contentContainerStyle={{minHeight: "100%", paddingBottom: 10}}>
                 <Image source={icons.logo} className="w-12 h-12 mt-20 mb-5 mx-auto"/>
